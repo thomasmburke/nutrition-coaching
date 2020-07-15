549142b38bd175601bbf0e17e57c7a5526dbd112
thomasmburke <tburke@bu.edu>
2020-07-14 22:49:45 -0700

    get_latest_date_in_sheet() added

--- src/googlesheets.py
+++ src/googlesheets.py
@@ -1,6 +1,7 @@
 import pygsheets
 import logging
 import os
+from datetime import datetime as dt
 
 # Set logger
 logger = logging.getLogger(__name__)
@@ -15,3 +16,18 @@ def initialize_gsheet_client() -> "<class 'pygsheets.client.Client'>":
     '''
     logger.info('Initializing Google Sheet Client...')
     return pygsheets.authorize(service_file=os.getenv('MFP_SA_PATH'), scopes=SCOPES)
+
+
+def get_latest_date_in_sheet(worksheet: "<class 'pygsheets.worksheet.Worksheet'>") -> "<class 'datetime.date'>":
+    """
+    Summary: Gets the latest date for a given worksheet. Assumes that the latest date is the first in the date column
+    """
+    latestDateString = worksheet.get_value('A2')
+    try:
+        latestDate = dt.date(dt.strptime(latestDateString, "%Y-%m-%d"))
+        logger.info(f"latest date in sheet: {latestDate}")
+        return latestDate
+    except Exception as e:
+        logger.error(
+            f"Was not able to convert latestDateString: {latestDateString} to a datetime.date()")
+        raise ValueError(str(e))
