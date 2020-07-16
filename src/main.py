f07e9cc2889920fa815e99b4a0ace5b2c3faa816
thomasmburke <tburke@bu.edu>
2020-07-15 21:43:56 -0700

    added check to see if row should be inserted

--- src/main.py
+++ src/main.py
@@ -26,19 +26,27 @@ logger.info(f"Yeserday's date: {yesterday}")
 mfpClient = initialize_mfp_client()
 # Initialize Google Sheets Client
 gsClient = initialize_gsheet_client()
-# Get all ordered information for a particular day
-# mfpData = get_ordered_mfp_dict(mfpClient, yesterday)
-mfpData = get_multiple_mfp_dicts(mfpClient=mfpClient, day=yesterday, numDays=2)
 # Get a reference to the nutrition coaching spreadsheet
 spreadsheet = gsClient.open_by_key(
     "1dgwN6dEDscQTOGpIE0tuEt4uvSPb3aQciGuizrxm9iI")
 # Select a specific worksheet from the spreadsheet
 worksheet = spreadsheet.worksheet(property='title', value=USER)
+# Get all ordered information for a particular day and numDays before it
+mfpData = get_multiple_mfp_dicts(mfpClient=mfpClient, day=yesterday, numDays=3)
 # Load OrderedDict into a Dataframe
-# mfpDf = pd.DataFrame([mfpData], columns=mfpData.keys())
 mfpDf = pd.DataFrame(mfpData, columns=mfpData[0].keys())
-print(mfpDf)
-# Inserts a row underneath the column headers
-worksheet.insert_rows(row=1, number=1, values=None)
+logger.info(mfpDf)
+latestDate = get_latest_date_in_sheet(worksheet=worksheet)
+# If we already have data for the day we are pulling then we should just overwrite it o/w we need to make space for the new row
+if (latestDate < yesterday):
+    logger.info(
+        f"yesterday: {yesterday} is < the latest date in the worksheet: {latestDate}, therefore we will insert a row.")
+    # Inserts a row underneath the column headers
+    worksheet.insert_rows(row=1, number=1, values=None)
+else:
+    logger.info(
+        f"yesterday: {yesterday} is not < the latest date in the worksheet: {latestDate}, therefore we will not insert a row.")
 # Inserts dailyTotalsDf in worksheet starting from A2
 worksheet.set_dataframe(mfpDf, 'A2', copy_head=False)
+# print(val)
+# gsClient.create
