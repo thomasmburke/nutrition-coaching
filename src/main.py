9745a5f57e02772616e8c3b4e6b4bce178f5ad81
thomasmburke <tburke@bu.edu>
2020-07-21 21:26:30 -0700

    automated the population of a weekly view

--- src/main.py
+++ src/main.py
@@ -13,7 +13,58 @@ from googlesheets import *
 # gcloud functions deploy pull-mfp-data --entry-point pull_mfp_data --runtime python37 --trigger-topic mfp-1-topic --timeout 540s --env-vars-file .env.yaml --project <PROJECT_ID>
 
 
-def pull_mfp_data(event, context):
+def daily_mfp_data_update(user: "str", mfpClient: "<class 'myfitnesspal.client.Client'>", gsClient: "< class 'pygsheets.client.Client' >") -> "<class 'pandas.Dataframe'>":
+    """
+    Summary: Pulls the last 3 days of data for a given user and updates their Google Sheet
+    """
+    # Get yesterday's date
+    yesterday = dt.date(dt.now() - timedelta(1))
+    logger.info(f"Yeserday's date: {yesterday}")
+    # Get a reference to the nutrition coaching spreadsheet
+    spreadsheet = gsClient.open(title=user)
+    # Select a specific worksheet from the spreadsheet
+    nutritionHistoryWorksheet = spreadsheet.worksheet(
+        property='title', value=f"{user}_NUTRITION_HISTORY")
+    # Get all ordered information for a particular day and numDays before it
+    mfpData = get_multiple_mfp_dicts(
+        mfpClient=mfpClient, day=yesterday, numDays=3)
+    # Load OrderedDict into a Dataframe
+    mfpDf = pd.DataFrame(mfpData, columns=mfpData[0].keys())
+    logger.info(mfpDf)
+    latestDate = get_latest_date_in_sheet(worksheet=nutritionHistoryWorksheet)
+    # If we already have data for the day we are pulling then we should just overwrite it o/w we need to make space for the new row
+    if (latestDate < yesterday):
+        logger.info(
+            f"yesterday: {yesterday} is < the latest date in the worksheet: {latestDate}, therefore we will insert a row.")
+        # Inserts a row underneath the column headers
+        nutritionHistoryWorksheet.insert_rows(row=1, number=1, values=None)
+    else:
+        logger.info(
+            f"yesterday: {yesterday} is not < the latest date in the worksheet: {latestDate}, therefore we will not insert a row.")
+    # Inserts dailyTotalsDf in worksheet starting from A2
+    nutritionHistoryWorksheet.set_dataframe(mfpDf, 'A2', copy_head=False)
+    return mfpDf
+
+
+def copy_weekly_view(user: "str", gsClient: "< class 'pygsheets.client.Client' >") -> "<class 'pandas.Dataframe'>":
+    """
+    Summary: Copy data from a user's nutrition history to a user's weekly view
+    """
+    spreadsheet = gsClient.open(title=user)
+    # Get a reference to the nutrition history worksheet
+    nutritionHistoryWorksheet = spreadsheet.worksheet(
+        property='title', value=f"{user}_NUTRITION_HISTORY")
+    # 8 rows because the first is the header, then grab all active columns
+    weekDf = nutritionHistoryWorksheet.get_as_df(
+        start='A1', end=(8, nutritionHistoryWorksheet.cols))
+    # Get a reference to the weekly worksheet
+    weeklyWorksheet = spreadsheet.worksheet(
+        property='title', value=f"{user}_WEEKLY")
+    weeklyWorksheet.set_dataframe(weekDf, 'A1')
+    return weekDf
+
+
+def pull_mfp_data(event: "dict", context: "google.cloud.functions.Context"):
     """Triggered from a message on a Cloud Pub/Sub topic.
     Args:
          event (dict): Event payload.
@@ -32,37 +83,13 @@ def pull_mfp_data(event, context):
     # Get user from pub/sub message
     USER = base64.b64decode(event['data']).decode('utf-8')
     logger.info(f"USER from pub/sub message: {USER}")
-    # Get yesterday's date
-    yesterday = dt.date(dt.now() - timedelta(1))
-    logger.info(f"Yeserday's date: {yesterday}")
     # Create client for communication with myfitnesspal
     mfpClient = initialize_mfp_client(USER)
     # Initialize Google Sheets Client
     gsClient = initialize_gsheet_client()
-    # Get a reference to the nutrition coaching spreadsheet
-    spreadsheet = gsClient.open(title=USER)
-    # spreadsheet = gsClient.open_by_key(
-    #     "1dgwN6dEDscQTOGpIE0tuEt4uvSPb3aQciGuizrxm9iI")
-    # Select a specific worksheet from the spreadsheet
-    worksheet = spreadsheet.worksheet(property='title', value=USER)
-    # Get all ordered information for a particular day and numDays before it
-    mfpData = get_multiple_mfp_dicts(
-        mfpClient=mfpClient, day=yesterday, numDays=3)
-    # Load OrderedDict into a Dataframe
-    mfpDf = pd.DataFrame(mfpData, columns=mfpData[0].keys())
-    logger.info(mfpDf)
-    latestDate = get_latest_date_in_sheet(worksheet=worksheet)
-    # If we already have data for the day we are pulling then we should just overwrite it o/w we need to make space for the new row
-    if (latestDate < yesterday):
-        logger.info(
-            f"yesterday: {yesterday} is < the latest date in the worksheet: {latestDate}, therefore we will insert a row.")
-        # Inserts a row underneath the column headers
-        worksheet.insert_rows(row=1, number=1, values=None)
-    else:
-        logger.info(
-            f"yesterday: {yesterday} is not < the latest date in the worksheet: {latestDate}, therefore we will not insert a row.")
-    # Inserts dailyTotalsDf in worksheet starting from A2
-    worksheet.set_dataframe(mfpDf, 'A2', copy_head=False)
-    # print(val)
-    # gsClient.create
-    # mfp-1-topic
+
+    # Daily update from MFP
+    daily_mfp_data_update(user=USER, mfpClient=mfpClient, gsClient=gsClient)
+
+    # Copy last week to the WEEKLY worksheet
+    copy_weekly_view(user=USER, gsClient=gsClient)
