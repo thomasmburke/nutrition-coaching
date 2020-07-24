9ba8ad2ddb7a15f5d156851d7d8b9b0c07e4e24c
thomasmburke <tburke@bu.edu>
2020-07-24 08:28:10 -0700

    outlining the work to be done when adding a new client

--- new_client.py
+++ new_client.py
@@ -8,16 +8,17 @@ import re
 
 
 def add_new_client(user: "str", email: "str") -> "pygsheets.Spreadsheet":
-    USER = "TBURKE"
     gsClient = initialize_gsheet_client()
     # Create a new spreadsheet for the new user
     # Folder in Margaux's Drive
-    gsClient.create(title=USER, folder="1ayQ9ERDdEdIgiG8AWHrJ7lbtiJtCLRdk")
+    gsClient.create(title=user, folder="1ayQ9ERDdEdIgiG8AWHrJ7lbtiJtCLRdk")
     # Get a reference to the newly created spreadsheet
-    spreadsheet = gsClient.open(title=USER)
+    spreadsheet = gsClient.open(title=user)
     # Add expected worksheets
-    spreadsheet.sheet1.title = f"{USER}_WEEKLY"
-    spreadsheet.add_worksheet(title=f"{USER}_NUTRITION_HISTORY")
+    spreadsheet.sheet1.title = f"{user}_WEEKLY"
+    spreadsheet.add_worksheet(title=f"{user}_NUTRITION_HISTORY")
+    # TODO: Add column headers to the nutrition history view
+
     # Share the spreadsheet with the new user's email
     spreadsheet.share(email_or_domain=email, role='reader', type='user',
                       emailMessage="Welcome to Margaux Carle's nutrition coaching program!\n\nWe will use this Google Sheet to track your nutrition info you provide in MyFitnessPal.")
@@ -53,12 +54,25 @@ class EmailType(object):
 
 
 def main():
+    # Gather args and validate them
     parser = argparse.ArgumentParser()
+    parser.add_argument('--user')
     parser.add_argument('--email', type=EmailType('RFC5322'))
     args = parser.parse_args()
+    USER = args.user.upper()
+    EMAIL = args.email
 
-    print(args.email)
+    # Set env vars
+    # This should call function from another file that will be in the .gitignore
+
+    # redeploy function
+
+    # create new scheduler job
+
+    # Create, format and share spreadsheet
+    # add_new_client(user=USER, email=EMAIL)
 
 
 if __name__ == '__main__':
+    # usage: python3 new_client.py --user tburke --email info@example.com
     main()
